b65086b3b168a102c524ed008e70aa1005a95c3a
Fantom52r <fantom52rs@gmail.com>
2024-09-24 13:11:50 +0300

    24/09/2024/ HW-2/Done

--- src/pages/LeaderBoard/LeaderBoard.jsx
+++ src/pages/LeaderBoard/LeaderBoard.jsx
@@ -4,14 +4,20 @@ import styles from "./LeaderBoard.module.css";
 import { getLeaders } from "../../API/leaders";
 import { Link } from "react-router-dom";
 
-// const COLUMNS_NAME = ["Позиция","Пользователь","Время"]
 const LeaderBoard = () => {
   const [leaders, setLeaders] = useState([{ position: "Позиция", name: "Пользователь", time: "Время" }]);
   useEffect(() => {
     getLeaders().then(data => {
-      setLeaders([...leaders, ...data.sort((a, b) => a - b)]);
+      setLeaders([...leaders, ...data.sort((a, b) => a.time - b.time)]);
     });
   }, []);
+
+  const timeFormat = digit => {
+    let minutes = Math.floor(digit / 60);
+    let seconds = digit % 60;
+    return [minutes < 10 ? "0" + minutes : minutes, ":", seconds < 10 ? "0" + seconds : seconds];
+  };
+
   return (
     <div className={styles.leaderBoardBlock}>
       <div className={styles.leaderBoarHeader}>
@@ -27,7 +33,7 @@ const LeaderBoard = () => {
               <ul className={styles.playerStatistic}>
                 <li className={styles.firstColumn}>{index === 0 ? player.position : player.id}</li>
                 <li className={styles.secondColumn}>{player.name}</li>
-                <li className={styles.thirdColumn}>{player.time}</li>
+                <li className={styles.thirdColumn}>{index === 0 ? player.time : timeFormat(player.time)}</li>
               </ul>
             </li>
           ))}
